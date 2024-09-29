8b68e80200751f4eae66fc57382ad63cd17723fd
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-09-29 02:30:55 -0400

    update footer

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -1,10 +1,22 @@
-<div class="h-px bg-neutral-200 dark:bg-neutral-500 border-0" />
-<div class="content w-full flex font-bold justify-between items-center p-8">
-    <p>Steelhacks XI 2024 - Developed by Scott Styslinger and Liam Sullivan</p>
-    <a href="https://github.com/liambsulliva/rewilding-project" target="_blank" rel="noreferrer">Source</a>
-</div>
-<style>
-    .content {
-        font-family: 'Geist Mono', Helvetica, sans-serif;
+<footer class="footer">
+    <div class="h-px bg-neutral-200 dark:bg-neutral-500 border-0" />
+    <div class="content w-full flex font-bold justify-between items-center p-8">
+        <p>Steelhacks XI 2024 - Developed by Scott Styslinger and Liam Sullivan</p>
+        <a href="https://github.com/liambsulliva/rewilding-project" target="_blank" rel="noreferrer">Source</a>
+    </div>
+</footer>
+  
+  <style>
+    :global(body) {
+      padding-bottom: 4rem;
     }
-</style>
+    .footer {
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+    }
+    .content {
+      font-family: 'Geist Mono', Helvetica, sans-serif;
+    }
+  </style>
